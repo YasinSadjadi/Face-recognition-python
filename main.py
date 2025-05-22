c484be1f3a7eab115be15cbcf9671b4a8faf7d8a
seyed yasin seyed sadjadi <yasin.sadjadi@gmail.com>
2025-05-22 14:07:35 +0330

    just code cleanup

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ class Main:
         self.state = States.TRAINING
         self.tts = TTS(model_name="tts_models/en/ljspeech/vits", progress_bar=False, gpu=True)
 
+        self.enter_your_name = self.tts.tts("if you want to train, enter your name, else press enter")
+        self.press_button_train = self.tts.tts("press 't' to train, press 'Esc' to exit")
+        self.press_button_recognize = self.tts.tts("if you want training press 't', and if not, press 'Esc' to exit")
+
 
     def run(self, default_state=States.TRAINING):
         self.state = default_state
@@ -41,35 +45,37 @@ class Main:
                 self.recognizing()
 
     def training(self):
-        tts = self.tts.tts("if you want to train, enter your name, else press enter")
-        sd.play(tts, samplerate=Main.SAMPLE_RATE)
+
+        sd.play(self.enter_your_name, samplerate=Main.SAMPLE_RATE)
         name = input("if you want to train, enter your name, else press enter: ")
 
         if name == "":
             self.transition(States.RECOGNIZING)
             return
 
-        tts = self.tts.tts("press 't' to train, press 'Esc' to exit")
-        sd.play(tts, samplerate=Main.SAMPLE_RATE)
-        sd.wait()
+
+        sd.play(self.press_button_train, samplerate=Main.SAMPLE_RATE)
         print("press 't' to train, press 'Esc' to exit")
+
         read = -1
         while True:
             frame = self.cap.read()
             if read == 27:
                 self.transition(States.RECOGNIZING)
                 break
+
             elif read == ord('t') or read == ord('T'):
                 self.model.train(user_id=name, image=frame)
+
             cv.imshow("frame", frame)
             read = cv.waitKey(1)
 
 
     def recognizing(self):
-        tts = self.tts.tts("if you want training press 't', and if not, press 'Esc' to exit")
-        sd.play(tts, samplerate=Main.SAMPLE_RATE)
-        # sd.wait()
+
+        sd.play(self.press_button_recognize, samplerate=Main.SAMPLE_RATE)
         print("if you want training press 't', and if not, press 'Esc' to exit")
+
         read = -1
         while True:
             frame = self.cap.read()
@@ -86,20 +92,24 @@ class Main:
                     continue
                 audio_array = self.tts.tts(f"hello {user}")
                 sd.play(audio_array, samplerate=Main.SAMPLE_RATE)
-                # sd.wait()
 
             for result in res:
                 if result[0] == "unknown":
                     continue
                 panel_center = []
+
                 face_center, face_width, face_height = result[2]
+
                 panel_center.append(int(face_center[0] + round(face_width * 1.5)))
                 panel_center.append(int(face_center[1] + round(face_height * 1.5)))
 
                 panel_first = (panel_center[0] - int(face_width * 1.4), panel_center[1] - face_height)
                 panel_second = (panel_center[0] + face_width, panel_center[1] + face_height)
+
                 cropped_panel = frame[panel_first[1]:panel_second[1], panel_first[0]:panel_second[0]]
+
                 cropped_panel = self.hand_detector.detect_hand(cropped_panel)
+
                 frame[panel_first[1]:panel_second[1], panel_first[0]:panel_second[0]] = cropped_panel
                 cv.rectangle(frame, panel_first, panel_second, (0, 0, 255), 2)
 
